e82225c7abb1eb27dc8981d25f13613d2e81b337
whcater <1010127369@qq.com>
2025-03-21 23:01:52 +0800

    补充上一次提交

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,5 +1,5 @@
 /** @type {import('tailwindcss').Config} */
-export default {
+module.exports = {
   content: [
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
@@ -28,5 +28,4 @@ export default {
   },
   darkMode: 'class',
   plugins: [],
-}
-
+} 
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,30 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import { resolve } from 'path';
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  root: './',
+  publicDir: 'public',
+  build: {
+    outDir: 'dist',
+  },
+  resolve: {
+    alias: {
+      '@': resolve(__dirname, './src')
+    },
+    extensions: ['.mjs', '.js', '.jsx', '.ts', '.tsx', '.json']
+  },
+  server: {
+    port: 3000,
+    open: true,
+    proxy: {
+      '/api': {
+        target: 'http://127.0.0.1:8787',
+        changeOrigin: true,
+        // 如果后端接口没有/api前缀，可以通过rewrite去掉
+        // rewrite: (path) => path.replace(/^\/api/, '')
+      }
+    }
+  },
+}); 
